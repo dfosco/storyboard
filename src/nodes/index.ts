f6212dd6f5732950acb08eb95503f997fc149268
Daniel Fosco <dfosco@github.com>
2024-04-28 18:44:06 +0200

    Add text-updater example node

--- src/nodes/index.ts
+++ src/nodes/index.ts
@@ -1,8 +1,13 @@
 import type { Node, NodeTypes } from "reactflow";
 import { PositionLoggerNode } from "./PositionLoggerNode";
+import { TextUpdaterNode } from "./TextUpdaterNode";
 
 export const initialNodes = [
-  { id: "a", type: "input", position: { x: 0, y: 0 }, data: { label: "wire" } },
+  { id: "a", 
+    type: "input", 
+    position: { x: 200, y: 0 }, 
+    data: { label: "wire" } 
+  },
   {
     id: "b",
     type: "position-logger",
@@ -15,10 +20,21 @@ export const initialNodes = [
     type: "output",
     position: { x: 0, y: 200 },
     data: { label: "with React Flow" },
+  },  {
+    id: "e",
+    type: "text-updater",
+    position: { x: 0, y: 200 },
+    data: { label: "Text Updater" },
   },
+
 ] satisfies Node[];
+
+
+// const nodeTypes = useMemo(() => ({ textUpdater: TextUpdaterNode }), []);
+
 
 export const nodeTypes = {
   "position-logger": PositionLoggerNode,
+  "text-updater": TextUpdaterNode,
   // Add any of your custom nodes here!
 } satisfies NodeTypes;
